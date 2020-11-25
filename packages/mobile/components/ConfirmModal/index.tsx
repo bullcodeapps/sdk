037ba7e55d8d0b126a2915687975e42108ff44aa
Gabriel Felipe <Gabriel_fcs@outlook.com>
2020-11-25 12:53:21 -0300

    fix(confirm-modal): allows changing the color of the buttons

--- packages/mobile/components/ConfirmModal/index.tsx
+++ packages/mobile/components/ConfirmModal/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import { Container, Title, Description, ActionButtons, CancelButton, ConfirmButton } from './styles';
 import { GestureResponderEvent } from 'react-native';
-import { useModal } from '../Modal';
+import { useModal } from '@bullcode/mobile';
 import { DefaultContainer } from '../../global-styles';
 
 export type ConfirmModalProps = {
@@ -12,6 +12,8 @@ export type ConfirmModalProps = {
   loading?: boolean;
   cancelText?: string;
   confirmText?: string;
+  cancelButtonColor?: string;
+  confirmButtonColor?: string;
   onCancel?: (event: GestureResponderEvent) => void;
   onConfirm?: (event: GestureResponderEvent) => void;
 };
@@ -19,10 +21,11 @@ export type ConfirmModalProps = {
 const ConfirmModal = ({
   title: paramTitle,
   description,
-  danger,
   loading,
   cancelText,
   confirmText,
+  cancelButtonColor,
+  confirmButtonColor,
   onCancel,
   onConfirm,
 }: ConfirmModalProps) => {
@@ -53,10 +56,10 @@ const ConfirmModal = ({
         <Title>{title}</Title>
         {description && <Description>{description}</Description>}
         <ActionButtons>
-          <CancelButton outline color="tertiary" onPress={handleOnCancel}>
+          <CancelButton outline color={cancelButtonColor || 'cancel'} onPress={handleOnCancel}>
             {(cancelText || 'Cancelar').toLowerCase()}
           </CancelButton>
-          <ConfirmButton loading={loading} color={danger ? 'danger' : null} onPress={handleOnConfirm}>
+          <ConfirmButton loading={loading} color={confirmButtonColor || 'danger'} onPress={handleOnConfirm}>
             {(confirmText || 'Confirmar').toLowerCase()}
           </ConfirmButton>
         </ActionButtons>
