427770617f799126c62dcf8e3920c8410ca54e80
Gabriel Felipe <Gabriel_fcs@outlook.com>
2020-12-02 17:54:43 -0300

    fix(date-time-picker): reverses the date time picker from the web that was committed by mistake

--- packages/web/components/Form/DateTimePicker.tsx
+++ packages/web/components/Form/DateTimePicker.tsx
@@ -1,379 +1,85 @@
-import React, { useState, useEffect, useCallback, useRef, useMemo, useContext } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { DateTimePicker as MUIDateTimePicker } from '@material-ui/pickers';
 
-import {
-  FieldType,
-  ViewContainer,
-  Input,
-  ModalViewTop,
-  ModalViewMiddle,
-  ActionText,
-  ModalViewBottom,
-  ClockIcon,
-  CalendarIcon,
-} from './styles';
-import RNDateTimePicker from '@react-native-community/datetimepicker';
-import { Platform, View, TouchableOpacity, Modal, ViewStyle } from 'react-native';
-import { InputProps, InputContextType, InputContext } from '../Form/Input';
-import {
-  format,
-  isAfter,
-  isBefore,
-  isEqual,
-  isWithinInterval,
-  parseISO,
-  toDate,
-  isValid as isValidDate,
-} from 'date-fns';
 import { useField } from '@unform/core';
-import { InputStyle, DefaultColors } from '@bullcode/mobile/components/Form/Input/styles';
-import { SvgProps } from 'react-native-svg';
+import { parse } from 'date-fns';
+import { FormControl } from './styles';
 
-export type DateTimePickerProps = {
-  name?: string;
-  mode?: 'date' | 'time';
-  color?: string;
-  placeholder?: string;
-  displayFormat?: string;
-  value?: Date;
-  doneText?: string;
-  clearText?: string;
-  maxDate?: Date;
-  minDate?: Date;
-  minuteInterval?: 1 | 2 | 3 | 4 | 5 | 6 | 10 | 12 | 15 | 20 | 30;
-  language?: string;
-  inputProps?: InputProps;
-  icon?: any;
-  style?: ViewStyle,
-  pickerStyle?: any,
-  onChange?: (value: Date) => void;
-};
+interface Props {
+  name: string;
+  label?: string;
+  helperText?: string;
+  autoOk?: boolean;
+  clearable?: boolean;
+  disabled?: boolean;
+  disableFuture?: boolean;
+  disablePast?: boolean;
+  value?: any;
+  onChange?: (date: Date) => void;
+}
 
-export type DateTimePickerComponent = React.FC<DateTimePickerProps>;
-
-const DateTimePicker: DateTimePickerComponent = ({
+export default function DateTimePicker({
   name,
-  mode = 'date',
-  color,
-  placeholder = 'date',
+  label,
+  helperText,
+  autoOk = true,
+  clearable = false,
+  disabled = false,
+  disableFuture = false,
+  disablePast = false,
   value,
-  doneText = 'Done',
-  clearText = 'Clear',
-  displayFormat,
-  inputProps,
-  maxDate,
-  minDate,
-  language = 'pt-BR',
-  icon: Icon,
-  style,
-  pickerStyle,
   onChange,
-  ...rest
-}) => {
-  const ctx = useContext<InputContextType>(InputContext);
-
-  const animationType = 'slide';
-
-  // States
-  const [date, setDate] = useState<Date | null>(null);
-  const [show, setShow] = useState(false);
-  const { fieldName, registerField, error } = useField(name);
-
-  // Refs
-  const inputRef = useRef<FieldType>(null);
-
-  const usingValidity = useMemo(() => ![undefined, null].includes(inputProps?.validity), [inputProps?.validity]);
-
-  const onChangeValue = useCallback(
-    ({ currentDate, validate = true }: { currentDate: any; validate?: boolean }) => {
-      const newMinDate = typeof minDate === 'string' ? new Date(minDate) : minDate;
-      const newMaxDate = typeof maxDate === 'string' ? new Date(maxDate) : maxDate;
-      const newDate = typeof currentDate === 'string' ? new Date(currentDate) : currentDate;
-
-      if (minDate && maxDate && isWithinInterval(newDate, { start: newMinDate, end: newMaxDate })) {
-        setDate(newDate);
-        onChange && onChange(newDate);
-        if (validate) {
-          inputRef?.current?.validate && inputRef.current.validate(newMaxDate);
-        }
-        return newDate;
-      }
-
-      if (minDate && isBefore(newDate, newMinDate)) {
-        setDate(newMinDate);
-        onChange && onChange(newMinDate);
-        if (validate) {
-          inputRef?.current?.validate && inputRef.current.validate(newMinDate);
-        }
-        return newMinDate;
-      }
-
-      if (maxDate && isAfter(newDate, newMaxDate)) {
-        setDate(newMaxDate);
-        onChange && onChange(newMaxDate);
-        if (validate) {
-          inputRef?.current?.validate && inputRef.current.validate(newMaxDate);
-        }
-        return newMaxDate;
-      }
-
-      setDate(newDate);
-      onChange && onChange(newDate);
-      if (validate) {
-        inputRef?.current?.validate && inputRef.current.validate(newDate);
-      }
-      return newDate;
-    },
-    [minDate, maxDate, onChange],
-  );
+  ...other
+}: Props) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const {
+    fieldName, registerField, defaultValue, error,
+  } = useField(name);
+  const [selected, setSelected] = useState<Date | null>(new Date());
 
   useEffect(() => {
-    if (!date || !value || !isValidDate(date) || !isValidDate(value)) {
-      return;
-    }
-    if (mode === 'date') {
-      const parsedDate = toDate(parseISO(format(date, 'yyyy-MM-dd')));
-      const parsedValue = toDate(parseISO(format(value, 'yyyy-MM-dd')));
-
-      if (isEqual(parsedDate, parsedValue)) {
-        return;
-      } else {
-        const newDate = typeof value === 'string' ? new Date(value) : value;
-        setDate(newDate);
-      }
-    }
-
-    if (mode === 'time') {
-      const parsedDateHour = toDate(parseISO(format(date, 'HH:mm')));
-      const parsedValueHour = toDate(parseISO(format(value, 'HH:mm')));
-
-      if (isEqual(parsedDateHour, parsedValueHour)) {
-        return;
-      } else {
-        const newDate = typeof value === 'string' ? new Date(value) : value;
-        setDate(newDate);
-      }
-    }
-  }, [date, mode, value]);
-
-  const getDefaultModeFormat = useCallback((mode: string): string => {
-    switch (mode) {
-      case 'time':
-        return 'HH:mm';
-      case 'date':
-      default:
-        return 'dd/MM/yyyy';
-    }
-  }, []);
-
-  const [choosenFormat, setChoosenFormat] = useState<string>(displayFormat || getDefaultModeFormat(mode));
-
-  const getDateFormatted = useCallback(
-    (dateObject: Date) => {
-      if (!dateObject || !isValidDate(dateObject) || !choosenFormat?.toString()) {
-        return;
-      }
-      if (typeof dateObject === 'string') {
-        dateObject = new Date(dateObject);
-      }
-      return format(dateObject, choosenFormat.toString(), { locale: (window as any).__dateLocale__ });
-    },
-    [choosenFormat],
-  );
+    setSelected(defaultValue || value as Date);
+  }, [defaultValue, value]);
 
   useEffect(() => {
-    if (!displayFormat) {
-      setChoosenFormat(getDefaultModeFormat(mode));
-    }
-  }, [displayFormat, getDefaultModeFormat, mode]);
-
-  const roundTime = (date: Date, minutesToRound: number) => {
-    // Convert hours and minutes to minutes
-    const time = date?.getHours() * 60 + date?.getMinutes();
-    const rounded = Math.ceil(time / minutesToRound) * minutesToRound;
-
-    const roundedHours = Math.floor(rounded / 60);
-    const roundedMinutes = rounded % 60;
-
-    const newDate = date;
-    newDate?.setHours(roundedHours);
-    newDate?.setMinutes(roundedMinutes);
-
-    return newDate;
-  };
-
-  useEffect(() => {
-    if (mode === 'time') {
-      if (rest?.minuteInterval && typeof value === 'object') {
-        setDate(roundTime(value, rest?.minuteInterval));
-        return;
-      }
-      setDate(value);
-    }
-  }, [mode, rest.minuteInterval, value]);
-
-  const clear = useCallback(() => {
-    // setDate(null);
-    setShow(false);
-    onChangeValue({ currentDate: null, validate: false });
-    // onChange && onChange(null);
-  }, [onChangeValue]);
-
-  useEffect(() => {
-    registerField<Date>({
+    registerField({
       name: fieldName,
       ref: inputRef.current,
-      clearValue: clear,
-      setValue: (ref: any, val: Date) => {
-        // Transforms dates from the form as a string to the data object automatically
-        const newDate = typeof val === 'string' ? new Date(val) : val;
-        setDate(newDate);
-        onChange && onChange(newDate);
-        inputRef?.current?.validate && inputRef.current.validate(newDate);
-      },
-      getValue: () => date,
+      path: 'value',
+      getValue: (ref: any) => (ref.value ? parse(ref.value, 'dd/MM/yyyy HH:mm', new Date()) : null),
+      clearValue: () => setSelected(defaultValue || null),
+      setValue: (ref: any, val: Date | string) => (val ? setSelected(new Date(val)) : null),
     });
-  }, [fieldName, registerField, date]); // eslint-disable-line
+  }, [inputRef.current, fieldName, defaultValue]); // eslint-disable-line
 
-  const selectDate = useCallback(
-    (event, newDate: Date) => {
-      if (!newDate || date === newDate) {
-        return;
-      }
-      setShow(Platform.OS === 'ios');
-      onChangeValue({ currentDate: newDate });
-    },
-    [date, onChangeValue],
-  );
+  const onChangeDateTime = (date: Date) => {
+    setSelected(date);
 
-  const togglePicker = useCallback(() => {
-    setShow(!show);
-    if (show) {
-      onChange && onChange(date || new Date());
-      inputRef?.current?.validate && inputRef.current.validate(date);
-    }
-  }, [date, onChange, show]);
-
-  const done = useCallback(() => {
-    setShow(false);
-    onChangeValue({ currentDate: date || new Date() });
-  }, [date, onChangeValue]);
-
-  const handleInputPress = useCallback(() => {
-    if (!date) {
-      onChangeValue({ currentDate: new Date() });
-    }
-    togglePicker();
-  }, [date, onChangeValue, togglePicker]);
-
-  const selectedColor: InputStyle = useMemo(() => {
-    const colors = ctx?.colors || DefaultColors;
-    const foundColor = colors.find((_color) => _color.name === color);
-    if (!foundColor) {
-      console.log(
-        `The "${color}" color does not exist, check if you wrote it correctly or if it was declared previously`,
-      );
-      return DefaultColors[0];
-    }
-    return foundColor;
-  }, [color, ctx?.colors]);
-
-  const getColorTypeByValidity = useCallback(
-    (validity?: boolean) => {
-      if (validity) {
-        return selectedColor?.valid || selectedColor?.default;
-      }
-      return selectedColor?.invalid || selectedColor?.default;
-    },
-    [selectedColor?.invalid, selectedColor?.valid, selectedColor?.default],
-  );
-
-  const currentValidationStyles = useMemo(() => {
-    if (usingValidity) {
-      if (inputProps?.validity === 'keepDefault') {
-        return selectedColor?.default;
-      }
-      return getColorTypeByValidity(inputProps?.validity);
-    }
-    if (date) {
-      return selectedColor?.valid || selectedColor?.default;
-    }
-
-    return selectedColor?.default;
-  }, [
-    error,
-    getColorTypeByValidity,
-    inputProps?.validity,
-    selectedColor?.default,
-    selectedColor?.invalid,
-    usingValidity,
-    date,
-  ]);
-
-  const DefaultIcon = useCallback(
-    (props: SvgProps) => (mode === 'time' ? <ClockIcon {...props} /> : <CalendarIcon {...props} />),
-    [mode],
-  );
+    onChange && onChange(date);
+  };
 
   return (
-    <ViewContainer style={style}>
-      <TouchableOpacity onPress={handleInputPress} activeOpacity={1}>
-        <View pointerEvents="none">
-          <Input
-            ref={inputRef}
-            name={`textDateTimePicker-${mode}${name ? `-${name}` : ''}`}
-            editable={false}
-            validity={date && !error}
-            placeholder={placeholder}
-            value={date && getDateFormatted(date)}
-            iconComponent={() => {
-              if (Icon) {
-                return <Icon color={currentValidationStyles?.borderColor} />;
-              }
-              return <DefaultIcon color={currentValidationStyles?.borderColor} />;
-            }}
-            color={color}
-            {...inputProps}
-          />
-        </View>
-      </TouchableOpacity>
-      <Modal
-        visible={show}
-        transparent
-        animationType={animationType}
-        supportedOrientations={['portrait', 'landscape']}
-      >
-        <ModalViewTop onPress={handleInputPress} />
-        {Platform.OS === 'ios' && (
-          <ModalViewMiddle>
-            <TouchableOpacity onPress={() => clear()} hitSlop={{ top: 4, right: 4, bottom: 4, left: 4 }}>
-              <View>
-                <ActionText allowFontScaling={false}>{clearText}</ActionText>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => done()} hitSlop={{ top: 4, right: 4, bottom: 4, left: 4 }}>
-              <View>
-                <ActionText allowFontScaling={false}>{doneText}</ActionText>
-              </View>
-            </TouchableOpacity>
-          </ModalViewMiddle>
-        )}
-        <ModalViewBottom>
-          <RNDateTimePicker
-            {...rest}
-            style={pickerStyle}
-            locale={language}
-            value={date}
-            maximumDate={maxDate}
-            minimumDate={minDate}
-            mode={mode as 'date' | 'time' | 'datetime' | 'countdown'}
-            onChange={selectDate}
-            display="spinner"
-            textColor="black"
-          />
-        </ModalViewBottom>
-      </Modal>
-    </ViewContainer>
-  );
-};
+    <FormControl error={!!error}>
+      {label && <label htmlFor={fieldName}>{label}</label>}
 
-export default DateTimePicker;
+      <MUIDateTimePicker
+        autoOk={autoOk}
+        clearable={clearable}
+        disabled={disabled}
+        disableFuture={disableFuture}
+        disablePast={disablePast}
+        ampm={false}
+        format="dd/MM/yyyy HH:mm"
+        name={fieldName}
+        value={value || selected}
+        inputRef={inputRef}
+        onChange={onChangeDateTime}
+        inputVariant="outlined"
+        size="small"
+        margin="dense"
+        helperText={error || helperText}
+        {...other}
+      />
+    </FormControl>
+  );
+}
