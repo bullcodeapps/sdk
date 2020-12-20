6103f2ca92d1ab74920abcb735b989cdc60d9a96
willferreiras <willferreiras1993@gmail.com>
2020-12-20 17:10:09 -0300

    refactor: creates global styles for section component

--- packages/mobile/components/Section/index.tsx
+++ packages/mobile/components/Section/index.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, {Ref,FunctionComponent, memo, useContext} from 'react';
 
-import { Container, SectionTitle } from './styles';
-import { TextStyle, ViewProps } from 'react-native';
+import { Container, SectionTitle, Content, SectiontStyles } from './styles';
+import { TextStyle, ViewProps, ViewStyle } from 'react-native';
 
-const Section = ({
+export type SectionContextType = { colors: SectiontStyles };
+
+export const SectionContext = React.createContext<SectionContextType>({ colors: null });
+
+export const setSectionStyles = () => {
+  useContext<SectionContextType>(SectionContext);
+};
+
+export type SectionRef = {};
+
+export interface SectionProps<T = any> {
+  ref?: Ref<SectionRef>;
+  outerRef?: Ref<SectionRef>;
+  containerStyle?: any;
+  contentContainerStyle?: ViewStyle;
+  title: string;
+  children: any;
+  titleStyle?: TextStyle;
+  containerProps?: ViewProps;
+  color?: string;
+}
+
+export type SectionComponent<T = any> = FunctionComponent<SectionProps<T>>;
+
+const Component: SectionComponent = ({
+  color = 'primary',
+  outerRef,
+  containerStyle,
+  contentContainerStyle,
+  containerProps,
   title,
   children,
   titleStyle,
   ...rest
-}: {
-  title: string;
-  children: any;
-  titleStyle?: TextStyle;
-} & ViewProps) => {
+}) => {
   return (
-    <Container {...rest}>
+    <Container style={containerStyle} {...containerProps}>
       {typeof title === 'string' ? <SectionTitle style={titleStyle}>{title}</SectionTitle> : { title }}
-      {children}
+      <Content style={contentContainerStyle}>
+        {children}
+      </Content>
     </Container>
   );
 };
 
-export default Section;
+const Section: SectionComponent = React.forwardRef((props: SectionProps, ref: Ref<SectionRef>) => (
+  <Component outerRef={ref} {...props} />
+));
+
+export default memo(Section);
--- packages/mobile/components/Section/styles.tsx
+++ packages/mobile/components/Section/styles.tsx
@@ -1,11 +1,37 @@
 import styled from 'styled-components/native';
+import { Animated } from 'react-native';
+
+export type SectionStateStyles = {
+  selectionColor: string,
+  placeholder: string,
+  color: string,
+  borderColor?: string,
+  backgroundColor?: string;
+  borderRadius?: number,
+  dropdownIconColor?: string;
+};
+
+export type SectiontStyle = {
+  name: string;
+  default: SectionStateStyles;
+  valid?: Partial<SectionStateStyles>;
+  invalid?: Partial<SectionStateStyles>;
+  disabled?: Partial<SectionStateStyles>;
+};
+
+export type SectiontStyles = Array<SectiontStyle>;
 
 export const Container = styled.View`
   flex-grow: 1;
   margin-bottom: 10px;
+  marginTop: 20px;
 `;
 
 export const SectionTitle = styled.Text`
   font-size: 16px;
   font-weight: 500;
+`;
+
+export const Content = styled(Animated.View)`
+  flex-grow: 1;
 `;
