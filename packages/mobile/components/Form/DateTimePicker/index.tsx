a9e881a8f4c30ad05e480cc1646df4ff0b3b7786
willferreiras <willferreiras1993@gmail.com>
2021-10-01 16:06:33 -0300

    feat: allow custom validation icons (#41)
    * allow to add icons to the beginning of inputs
    
    * refactor: remove unneeded parameters and refactor typing and styling
    
    * change input types nomenclature and fix layout
    
    * allow to inform an icon at the beginning of fields of type select
    
    * adapt dateTimePicker to changes made to the input field
    
    * fix: correctly apply the stylizations to the input
    
    * fix: layout corrections (input, phoneInput, select)
    
    * refactor: add floating label to input component
    
    * feat: allow custom validation icons
    
    * fix: fixes applied to code (css, useMemo)
    
    * refactor: adjustments for correct functioning in all projects
    
    Co-authored-by: Giorgio Fellipe <giorgiofellipe@gmail.com>

--- packages/mobile/components/Form/DateTimePicker/index.tsx
+++ packages/mobile/components/Form/DateTimePicker/index.tsx
@@ -397,7 +397,7 @@ const Component: DateTimePickerComponent = ({
     return response;
   }, [maxDate, minDate]);
 
-  const IconComponent = useCallback(
+  const IconComponent = useMemo(
     () => {
       if (!!Icon) {
         return <Icon color={currentValidationStyles?.borderColor} />;
